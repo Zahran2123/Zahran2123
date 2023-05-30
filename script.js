8608134a2a073bec93f8f5cb2f7a0c32c67a92e9
Zahran2123 <122143844+Zahran2123@users.noreply.github.com>
2023-05-30 22:27:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+alert("selamat datang di web TURNAMEN MOBILE LEGEND");
+
+const navbarNav = document.querySelector(".navbar-nav");
+document.querySelector("#menu-web").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
