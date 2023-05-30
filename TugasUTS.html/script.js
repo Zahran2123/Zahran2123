e60e23d4ab04d2f177c9a89459c2954f8dc1f541
Zahran2123 <122143844+Zahran2123@users.noreply.github.com>
2023-05-30 22:26:39 +0700

    Delete TugasUTS.html directory

--- TugasUTS.html/script.js
+++
@@ -1,6 +0,0 @@
-alert("selamat datang di web TURNAMEN MOBILE LEGEND");
-
-const navbarNav = document.querySelector(".navbar-nav");
-document.querySelector("#menu-web").onclick = () => {
-  navbarNav.classList.toggle("active");
-};
